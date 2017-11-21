44819bc4d19ca85677a7f5cb854570bf50569ecf
AntonioOsuna97 <31919715+AntonioOsuna97@users.noreply.github.com>
2017-11-21 16:43:04 +0100

    Finalizada práctica2_PPT
    Añadido fecha y completado dominio.
    Añadido una memoria de los pasos que hemos seguido para completar esta práctica.

--- cliente46.c
+++ cliente46.c
@@ -19,6 +19,8 @@ Autor: Juan Carlos Cuevas Martínez
 #include <ws2tcpip.h>//Necesaria para las funciones IPv6
 #include <conio.h>
 #include "protocol.h"
+//Libreria para tiempo
+#include <time.h>
 
 //Me permite ejecutar el archivo
 #pragma comment(lib, "Ws2_32.lib")
@@ -29,25 +31,34 @@ Autor: Juan Carlos Cuevas Martínez
 
 int main(int *argc, char *argv[])
 {
+	//Definición de variables
 	SOCKET sockfd; //Crea el socket
-	struct  hostent *host; //Estructura host (SESION 5)
+	//Estructura host (SESION 5)
+	struct  hostent *host; 
 	struct in_addr address; //SESION 5
 	struct sockaddr *server_in;
 	struct sockaddr_in server_in4;
 	struct sockaddr_in6 server_in6;
 	int address_size = sizeof(server_in4);
 	char buffer_in[1024], input[1024],input2[1024], input3[1024], input4[1024];
-	char buffer_out[6144]; //Suma de tamaños input, input2...
+	char buffer_out[6144]; //Buffer de salida le pondremos un tamaño grande.
 	int recibidos=0,enviados=0;
 	int estado=S_HELO;
 	char option;
 	int ipversion=AF_INET;//IPv4 por defecto
-	char ipdest[256];
-	char ipdestl="";
+	char ipdest[256]; //ipdestino
+	char ipdestl=""; //ipdestino utilizada para dominio
+	//Ips por defecto
 	char default_ip4[16]="127.0.0.1";
 	char default_ip6[64]="::1";
 	//Definimos un caracter para el bucle
 	char caracter="";
+
+	//Variables para la fecha
+	time_t tiempo = time(0);
+	struct tm *tlocal = localtime(&tiempo);
+	char salida_tiempo[128];
+	strftime(salida_tiempo, 128, "%d/%m/%y %H:%M",tlocal);
 
 	WORD wVersionRequested;
 	WSADATA wsaData;
@@ -67,12 +78,12 @@ int main(int *argc, char *argv[])
 	
 	printf("**************\r\nCLIENTE TCP SENCILLO SOBRE IPv4 o IPv6\r\n*************\r\n");
 	
-
+	
 	do{
 
 		printf("CLIENTE> ¿Qué versión de IP desea usar? 6 para IPv6, 4 para IPv4 [por defecto] ");
 		gets_s(ipdest, sizeof(ipdest));
-
+		//Si la opcion es 6, la familia sera ipv6
 		if (strcmp(ipdest, "6") == 0) {
 			ipversion = AF_INET6;
 			printf_s("Familia IPV6\r\n");
@@ -82,6 +93,7 @@ int main(int *argc, char *argv[])
 			printf_s("Familia IPV4\r\n");
 		}
 
+		//Crea el descriptor socket TCP, familia ipversion
 		sockfd=socket(ipversion,SOCK_STREAM,0);
 		if(sockfd==INVALID_SOCKET){
 			printf("CLIENTE> ERROR\r\n");
@@ -92,18 +104,18 @@ int main(int *argc, char *argv[])
 			printf("CLIENTE> Introduzca la IP destino (pulsar enter para IP por defecto) o dominio: ");
 			gets_s(ipdest,sizeof(ipdest));
 			//SESION 5
-			//Codigo profesor(No introducir dominio de argosoft)
+			//Codigo profesor diapositivas(No introducir dominio de argosoft)
 			ipdestl = inet_addr(ipdest);
 			if (ipdestl == INADDR_NONE) {
 				//La dirección introducida por teclado no es correcta o
 				//corresponde con un dominio.
 				struct hostent *host;
-				host = gethostbyname(ipdest);
-				if (host != NULL) {
-					memcpy(&address, host->h_addr_list[0], 4);
+				host = gethostbyname(ipdest); //Pruebo si es dominio
+				if (host != NULL) { //Si es distinto de null, es dominio
+					memcpy(&address, host->h_addr_list[0], 4);  //Tomo los 4 primeros bytes.
 					printf("\nDireccion %s\n", inet_ntoa(address));
-					//ipdest[256] = inet_ntoa(address);
 				}			
+				//Copia en ipdest
 				strcpy_s(ipdest,sizeof(ipdest),inet_ntoa(address));
 			}
 			
@@ -114,9 +126,11 @@ int main(int *argc, char *argv[])
 			*/
 
 			//Dirección por defecto según la familia
+			//Si es familia AF_INET --> Ipv4
 			if(strcmp(ipdest,"")==0 && ipversion==AF_INET)
 				strcpy_s(ipdest,sizeof(ipdest),default_ip4);
 
+			//Si es familia AF_INET6 --> Ipv6
 			if(strcmp(ipdest,"")==0 && ipversion==AF_INET6)
 				strcpy_s(ipdest, sizeof(ipdest),default_ip6);
 
@@ -140,6 +154,7 @@ int main(int *argc, char *argv[])
 			
 			estado=S_HELO;
 
+			//Comprobamos si se establece conexión
 			if(connect(sockfd, server_in, address_size)==0){
 				printf("CLIENTE> CONEXION ESTABLECIDA CON %s:%d\r\n",ipdest, default_mailPort);
 				recibidos = recv(sockfd, buffer_in, 512, 0); //recibimos la informacion del servidor para saber que esta listo
@@ -155,10 +170,9 @@ int main(int *argc, char *argv[])
 
 						//Pasamos al estado MAILFROM
 					case S_MAIL:
-						printf("MAIL FROM:");
+						printf("MAIL FROM: (enter para salir):");
 						//Lee los caracteres de la entrada estándar y los almacena como una  
-						//cadena C en str hasta que se alcanza un carácter de nueva línea
-						//str es un Puntero a un bloque de memoria (array de Char )
+						//cadena hasta que se alcanza un carácter de nueva línea
 						gets_s(input, sizeof(input));
 						if (strlen(input) == 0) {
 							// Si la longitud de input es 0, pasamos al estado QUIT
@@ -175,7 +189,7 @@ int main(int *argc, char *argv[])
 
 						//Pasamos al estado RCPT TO
 					case S_RCPT:
-						printf("RCPT TO: ");
+						printf("RCPT TO: (enter para salir)");
 						gets_s(input2, sizeof(input2));
 
 						if (strlen(input2) == 0) {
@@ -192,8 +206,10 @@ int main(int *argc, char *argv[])
 						break;
 
 					case S_DATA:
-						printf("CLIENTE> Introduzca datos (enter o QUIT para salir): ");
+						printf("CLIENTE> Introduzca datos (enter para salir): ");
+						//Introducimos datos
 						gets_s(input4, sizeof(input4));
+						//Si el tamaño de input4 es 0, nos salimos
 						if (strlen(input4) == 0) {
 							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", SD, CRLF);
 							estado = S_QUIT;
@@ -206,13 +222,16 @@ int main(int *argc, char *argv[])
 						break;
 
 					case S_MENSAJE:
+						//Escribimos asunto
 						printf("Asunto:");
 						gets(input3);
 						printf("\nMensaje de correo: \r\n");
+						printf("Fecha y hora: %s \r\n",salida_tiempo); //Nos da la hora en la que se ha enviado
 						printf("Asunto: %s%s", input3, CRLF);
 						printf("Remitente: %s%s%s", MA, input, CRLF);
 						printf("Destinatario: %s%s%s", RCPT, input2, CRLF);
 						printf("Datos: %s%s", input4, CRLF);
+						//Enviamos mensaje
 						enviados = send(sockfd, buffer_out, (int)strlen(buffer_out), 0);
 						printf("SERVIDOR> Datos enviados correctamente\r\n");
 						estado++;
@@ -223,15 +242,16 @@ int main(int *argc, char *argv[])
 							printf("¿Desea escribir otro mensaje? (s/n)\r\n");
 							caracter = _getche(); //Lee caracter
 						} while (caracter!= 's' && caracter!= 'n' && caracter!= 'S' && caracter!='N');
+						//Si es S o s pasamos al estado S_HELO y escribimos otro mensaje
 						if (caracter == 'S' || caracter == 's') {
 							estado = S_HELO;
 						}
 						else {
+							//Si no, pasamos al estado S_QUIT y nos salimos
 							estado++;
 						}
 						break;
 						
-						//No necesario introducir codigo. do{}while
 					case S_QUIT:
 
 						break;
@@ -262,8 +282,8 @@ int main(int *argc, char *argv[])
 						buffer_in[recibidos] = 0x00;
 						//Nos permite escribir un mensaje de envio correcto y recepcion correcta
 						//Ponemos los estados S_RCPT y S_DATA debido a que queremos que escriba el mensaje 
-						//en el estado mail y rcpt --> en mail estado++(RCPT) y rcpt estado++(DATA)
-						if (estado == S_RCPT || estado == S_DATA) {
+						//en el estado mail , rcpt y helo --> en helo estado++ (mail),en mail estado++(RCPT) y rcpt estado++(DATA)
+						if (estado == S_MAIL || estado == S_RCPT || estado == S_DATA) {
 							//Escribe un mensaje de envio correcto y recepción correcta
 							printf(buffer_in);
 							//Definimos en protocol.h una respuesta a un comando de aplicacion
