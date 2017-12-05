73b78659e4fd250a53782719b2b321ed309e602b
AntonioOsuna97 <31919715+AntonioOsuna97@users.noreply.github.com>
2017-12-05 17:38:14 +0100

    Práctica final
    En la validación de la práctica, Juan Carlos modificó el código para decidir si se quería enviar un mensaje o no. 
    Esto ha provocado algunos cambios, por ejemplo, si introducíamos dos destinatarios y decidíamos no enviar el mensaje, al introducir otros destinatarios nos escribía todos los escritos tanto en el primer intento como en el segundo. Para modificar esto, hemos introducido una función memset para vaciar el vector.
    En la primera validación (en clase), al apagar el servidor no nos permitía seguir los pasos para escribir un mensaje. Ahora, al hacer algunas modificaciones, al apagar el servidor nos permite seguir escribiendo. NOTA: Esta será nuestra practica final, a no ser que hoy se encuentre el fallo.

--- cliente46.c
+++ cliente46.c
@@ -43,7 +43,6 @@ int main(int *argc, char *argv[])
 	struct sockaddr_in6 server_in6;
 	int address_size = sizeof(server_in4);
 	char buffer_in[1024], input[1024],input2[1024], input3[1024], input4[1024],input5[4000], inputDat[1024]= "", inputprueba[128];
-	char input6[1024];
 	char buffer_out[9000]; //Buffer de salida le pondremos un tamaño grande.
 	int recibidos=0,enviados=0;
 	//Iniciamos el estado a S_HELO
@@ -51,13 +50,13 @@ int main(int *argc, char *argv[])
 	char option;
 	int ipversion=AF_INET;//IPv4 por defecto
 	char ipdest[256]; //ipdestino
-	char ipdestl=""; //ipdestino utilizada para dominio
+	char ipdestl; //ipdestino utilizada para dominio
 	//Ips por defecto
 	char default_ip4[16]="127.0.0.1";
 	char default_ip6[64]="::1";
 	//Definimos un caracter para el bucle
-	char caracter="";
-	char punto = "";
+	char caracter;
+
 
 	//Variables para la fecha
 	time_t tiempo = time(0);
@@ -104,26 +103,25 @@ int main(int *argc, char *argv[])
 			printf("CLIENTE> ERROR\r\n");
 			exit(-1);
 		}
-		else{
+		else {
 			printf_s("CLIENTE> Socket CREADO\n");
 			printf("CLIENTE> Introduzca la IP destino (pulsar enter para IP por defecto) o dominio: ");
-			gets_s(ipdest,sizeof(ipdest));
+			gets_s(ipdest, sizeof(ipdest));
 			//SESION 5
 			//Codigo profesor diapositivas(No introducir dominio de argosoft)
 			ipdestl = inet_addr(ipdest); // Convierte a dirección IP o a cadena 
 			if (ipdestl == INADDR_NONE) {
 				//La dirección introducida por teclado no es correcta o
 				//corresponde con un dominio.
-				struct hostent *host;
 				host = gethostbyname(ipdest); //Pruebo si es dominio
 				if (host != NULL) { //Si es distinto de null, es dominio
 					memcpy(&address, host->h_addr_list[0], 4);  //Tomo los 4 primeros bytes.
 					printf("Direccion %s\n", inet_ntoa(address));
-				}		
+				}
 				//Copia en ipdest
-				strcpy_s(ipdest,sizeof(ipdest),inet_ntoa(address));
+				strcpy_s(ipdest, sizeof(ipdest), inet_ntoa(address));
 			}
-			
+
 			/*MAL
 			if (strcmp(ipdest,"lib.simulacion") == 0) {
 				strcpy_s(ipdest, sizeof(ipdest), default_ip4);
@@ -132,46 +130,60 @@ int main(int *argc, char *argv[])
 
 			//Dirección por defecto según la familia
 			//Si es familia AF_INET --> Ipv4
-			if(strcmp(ipdest,"")==0 && ipversion==AF_INET)
-				strcpy_s(ipdest,sizeof(ipdest),default_ip4);
+			if (strcmp(ipdest, "") == 0 && ipversion == AF_INET)
+				strcpy_s(ipdest, sizeof(ipdest), default_ip4);
 
 			//Si es familia AF_INET6 --> Ipv6
-			if(strcmp(ipdest,"")==0 && ipversion==AF_INET6)
-				strcpy_s(ipdest, sizeof(ipdest),default_ip6);
+			if (strcmp(ipdest, "") == 0 && ipversion == AF_INET6)
+				strcpy_s(ipdest, sizeof(ipdest), default_ip6);
 
-			if(ipversion==AF_INET){
-				server_in4.sin_family=AF_INET;
-				server_in4.sin_port=htons(default_mailPort);
+			if (ipversion == AF_INET) {
+				server_in4.sin_family = AF_INET;
+				server_in4.sin_port = htons(default_mailPort);
 				//server_in4.sin_addr.s_addr=inet_addr(ipdest);
-				inet_pton(ipversion,ipdest,&server_in4.sin_addr.s_addr);
-				server_in=(struct sockaddr*)&server_in4;
+				inet_pton(ipversion, ipdest, &server_in4.sin_addr.s_addr);
+				server_in = (struct sockaddr*)&server_in4;
 				address_size = sizeof(server_in4);
 			}
 
-			if(ipversion==AF_INET6){
+			if (ipversion == AF_INET6) {
 				memset(&server_in6, 0, sizeof(server_in6));
-				server_in6.sin6_family=AF_INET6;
-				server_in6.sin6_port=htons(default_mailPort);
-				inet_pton(ipversion,ipdest,&server_in6.sin6_addr);
-				server_in=(struct sockaddr*)&server_in6;
+				server_in6.sin6_family = AF_INET6;
+				server_in6.sin6_port = htons(default_mailPort);
+				inet_pton(ipversion, ipdest, &server_in6.sin6_addr);
+				server_in = (struct sockaddr*)&server_in6;
 				address_size = sizeof(server_in6);
 			}
-			
-			estado=S_HELO;
+
+			estado = S_HELO;
 
 			//Comprobamos si se establece conexión
-			if(connect(sockfd, server_in, address_size)==0){
-				printf("CLIENTE> CONEXION ESTABLECIDA CON %s:%d\r\n",ipdest, default_mailPort);
+			if (connect(sockfd, server_in, address_size) == 0) {
+				printf("CLIENTE> CONEXION ESTABLECIDA CON %s:%d\r\n", ipdest, default_mailPort);
 				recibidos = recv(sockfd, buffer_in, 512, 0); //recibimos la informacion del servidor para saber que esta listo
-				//Inicio de la máquina de estados
+				//Hacemos un bucle de recibidos como en el caso de abajo
+				if (recibidos <= 0) {
+					DWORD error = GetLastError();
+					if (recibidos < 0) {
+						printf("CLIENTE> Error %d en la recepción de datos\r\n", error);
+						estado = S_QUIT;
+					}
+					else {
+						printf("CLIENTE> Conexión con el servidor cerrada\r\n");
+						estado = S_QUIT;
+					}
+				}
+				
+					//Inicio de la máquina de estados
 				do {
+					
 					switch (estado) {
 					case S_HELO:
 						// Se recibe el mensaje de bienvenida
 						printf("\nBienvenido a SEVICEMAIL-ANFE\r\n");
 						//sprintf_s da formato y almacena una serie de caracteres y valores en un buffer
 						//Escribe en el servidor HELO y el ipdest
-						sprintf_s(buffer_out, sizeof(buffer_out), "%s %s %s",HE, ipdest, CRLF); //250 correcto
+						sprintf_s(buffer_out, sizeof(buffer_out), "%s %s %s", HE, ipdest, CRLF); //250 correcto
 						estado++;
 						break;
 
@@ -208,7 +220,7 @@ int main(int *argc, char *argv[])
 							estado = S_QUIT;
 						}
 						else {
-							
+
 							//Escribe en el servidor RCPT TO: input y crlf y pasamos al siguiente estado
 							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s%s", RCPT, input2, CRLF);
 
@@ -228,9 +240,28 @@ int main(int *argc, char *argv[])
 						}
 						else { */
 
+						//Parte añadido en la validación de prácticas (Juan Carlos Cuevas)
+						//Necesitabamos pedir si queriamos enviar un mensaje o no
+						do {
+							printf("¿Desea enviar un mensaje? (s/n)\r\n");
+							caracter = _getche(); //Lee caracter
+						} while (caracter != 's' && caracter != 'n' && caracter != 'S' && caracter != 'N');
+						//Si es S o s pasamos al estado S_HELO y escribimos otro mensaje
+						if (caracter == 'S' || caracter == 's') {
+
 							//Escribe en el servidor DATA y pasamos al siguiente estado
 							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", DATA, CRLF);
 							estado++;
+						}
+						else
+						{
+
+							//Necesitamos vaciar la cadena de caracteres input2, para que no los escriba 
+							memset(inputDat, 0, sizeof(inputDat));
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", RSET, CRLF);
+							estado = S_HELO;
+
+						}
 						break;
 
 						//Mensaje
@@ -238,48 +269,47 @@ int main(int *argc, char *argv[])
 						printf("Escribe mensaje(enter y un punto para finalizar): ");
 						//Lee los caracteres de la entrada estándar y los almacena como una  
 						//cadena hasta que se alcanza un carácter de nueva línea
-						gets(input5,sizeof(input5));
+						gets(input5, sizeof(input5));
 						do {
-							gets(input4,sizeof(input4));
+							gets(input4, sizeof(input4));
 							//Concatenamos la cadena 5 con un \n debido al enter
 							strcat(input5, "\n");
 							//Concatenamos la cadena 5 con la cadena anteriormente introducida
 
 							strcat(input5, input4);
-						} while (strcmp(input4, ".")!=0);
-							
+						} while (strcmp(input4, ".") != 0);
+
 						//Escribimos asunto
 						printf("Asunto:");
 						gets(input3);
 						//Escribimos el mensaje en la pantalla de comandos 
 						printf("\nMensaje de correo: \r\n");
-						printf("Fecha y hora: %s \r\n",salida_tiempo); //Nos da la hora en la que se ha enviado
+						printf("Fecha y hora: %s \r\n", salida_tiempo); //Nos da la hora en la que se ha enviado
 						printf("Asunto: %s%s", input3, CRLF);
 						printf("Remitente: %s%s%s", MA, input, CRLF);
 						printf("Destinatario: %s%s%s", RCPT, inputDat, CRLF);
 						printf("Datos: %s%s", input5, CRLF);
 						//Escribe en el servidor
-						sprintf_s(buffer_out, sizeof(buffer_out), "Date:%s%s From:%s%s To:%s%s Subject:%s%s DATA: %s%s%s%s",salida_tiempo,CRLF,input,CRLF, inputDat, CRLF, input3, CRLF, input5, CRLF, input4, CRLF);
-						
+						sprintf_s(buffer_out, sizeof(buffer_out), "Date:%s%s From:%s%s To:%s%s Subject:%s%s DATA: %s%s%s%s", salida_tiempo, CRLF, input, CRLF, inputDat, CRLF, input3, CRLF, input5, CRLF, input4, CRLF);
+
 						estado++;
 						printf("SERVIDOR> Datos enviados correctamente\r\n");
-						
+
 						break;
 
 					case S_RSET:
 						do {
 							printf("¿Desea escribir otro mensaje? (s/n)\r\n");
 							caracter = _getche(); //Lee caracter
-						} while (caracter!= 's' && caracter!= 'n' && caracter!= 'S' && caracter!='N');
+						} while (caracter != 's' && caracter != 'n' && caracter != 'S' && caracter != 'N');
 						//Si es S o s pasamos al estado S_HELO y escribimos otro mensaje
 						if (caracter == 'S' || caracter == 's') {
-							//Necesitamos vaciar la cadena de caracteres inputDat, para que no escriba
+							//Necesitamos vaciar la cadena de caracteres inputDat, para que no los escriba 
 							memset(inputDat, 0, sizeof(inputDat));
 
 							//Escribe en el servidor RSET y pasamos al estado HELO
-							sprintf_s(buffer_out,sizeof(buffer_out),"%s%s", RSET,CRLF);
-							//Enviamos el mensaje mediante el comando send de TCP
-							enviados = send(sockfd, buffer_out, (int)strlen(buffer_out), 0);
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", RSET, CRLF);
+							//Asigna Estado a S_HELO
 							estado = S_HELO;
 						}
 						else {
@@ -288,38 +318,36 @@ int main(int *argc, char *argv[])
 							estado++;
 						}
 						break;
-						
+
 					case S_QUIT:
 
 						break;
-					}
+						}
 
-					//Enviamos el mensaje si es distinto de S_HELO
-					if(estado!=S_HELO){
-						enviados=send(sockfd,buffer_out,(int)strlen(buffer_out),0);
+
+						enviados = send(sockfd, buffer_out, (int)strlen(buffer_out), 0);
 						//Si el socket es erroneo nos vamos a S_QUIT
-						if(enviados==SOCKET_ERROR){
-							 estado=S_QUIT;
-							 continue;
+						if (enviados == SOCKET_ERROR) {
+							estado = S_QUIT;
+							continue;
 						}
-					}
 
 
-					//Recibe el mensaje
-					recibidos=recv(sockfd,buffer_in,512,0);
-					if(recibidos<=0){
-						DWORD error=GetLastError();
-						if(recibidos<0){
-							printf("CLIENTE> Error %d en la recepción de datos\r\n",error);
-							estado=S_QUIT;
+						//Recibe el mensaje
+						recibidos = recv(sockfd, buffer_in, 512, 0);
+						if (recibidos <= 0) {
+							DWORD error = GetLastError();
+							if (recibidos < 0) {
+								printf("CLIENTE> Error %d en la recepción de datos\r\n", error);
+								estado = S_QUIT;
+							}
+							else {
+								printf("CLIENTE> Conexión con el servidor cerrada\r\n");
+								estado = S_QUIT;
+							}
 						}
-						else{
-							printf("CLIENTE> Conexión con el servidor cerrada\r\n");
-							estado=S_QUIT;
-						}
-					}
-					else {
-						buffer_in[recibidos] = 0x00;
+						else {
+							buffer_in[recibidos] = 0x00;
 							//Escribe un mensaje proporcionado por el servidor
 							printf(buffer_in);
 							//Definimos en protocol.h una respuesta a un comando de aplicacion
@@ -327,12 +355,12 @@ int main(int *argc, char *argv[])
 							//Si el buffer_in (respuesta del servidor) es usuario erroneo
 							if (strncmp(buffer_in, UU, 2) == 0) {
 								//Definimos variable para introducir enteros
-								int estado2=0;
+								int estado2 = 0;
 								do {
 									printf("Introduce 1 --> Introducir los dos usuarios de nuevo, necesidad de reset\n2--> Para introducir un usuario correcto\n");
 									//scanf_s necesita el tipo de dato %i y direccion &.
 									scanf_s("%i", &estado2);
-									switch (estado2) {									
+									switch (estado2) {
 									case 1:
 										/* MAL -->  estado = S_MAIL; */
 										printf("Para introducir los dos nuevos usuario necesita confirmar que desea escribir mensaje nuevo.\n");
@@ -357,7 +385,7 @@ int main(int *argc, char *argv[])
 								} while (estado2 != 1 && estado2 != 2);
 							}
 							//A continuación, irá la parte que nos permitirá declarar más destinatarios, S_DATA debido a estado++
-							if(estado==S_DATA){
+							if (estado == S_DATA) {
 								int estado3 = 0;
 								do {
 									//strcat Agrega una copia de la cadena de origen a la cadena de destino
@@ -385,15 +413,16 @@ int main(int *argc, char *argv[])
 										printf("Opcion no disponible\n");
 										break;
 									}
-								} while (estado3 != 1 && estado3 != 2);  
-							} 
-						}
-				}while(estado!=S_QUIT);		
-			}
-			else{
-				int error_code=GetLastError();
-				printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n",ipdest, default_mailPort);
-			}		
+								} while (estado3 != 1 && estado3 != 2);
+							}
+					}
+					} while (estado != S_QUIT);
+				}else {
+					int error_code = GetLastError();
+					
+					printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n", ipdest, default_mailPort);
+				}
+			
 			// fin de la conexion de transporte
 			closesocket(sockfd);
 			
